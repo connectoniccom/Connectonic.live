14b7bf8288a781e356e1ef9e568e8a5a1fd3ba89
connectoniccom <Connectonic.com@gmail.com>
2025-11-04 01:20:05 +0300

    Delete src directory

--- src/ai/dev.ts
+++
@@ -1,6 +0,0 @@
-import { config } from 'dotenv';
-config();
-
-import '@/ai/flows/answer-technical-query.ts';
-import '@/ai/flows/summarize-documentation.ts';
-import '@/ai/flows/adapt-response-to-expertise-level.ts';
