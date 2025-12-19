d223c00c080c56f5be5c8b43369a533c5649cd06
matayobyabasajja77 <matayobyabasajja77@gmail.com>
2025-12-19 12:17:45 +0000

    Cleaned up project and simplified to Hello World, preparing for Telegram bot development

--- src/pages/_document.tsx
+++ src/pages/_document.tsx
@@ -3,10 +3,7 @@ import { Html, Head, Main, NextScript } from 'next/document';
 export default function Document() {
   return (
     <Html lang="en">
-      <Head>
-        <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.0.0-beta3/css/all.min.css" />
-         <script async src="https://pagead2.googlesyndication.com/pagead/js/adsbygoogle.js?client=ca-pub-2583060052563516" crossOrigin="anonymous" />
-      </Head>
+      <Head />
       <body>
         <Main />
         <NextScript />
